55f93c55b78732d815a796baa897bb6375981a8d
Austine-Antoni <168243971+Austine-Antoni@users.noreply.github.com>
2025-03-22 14:04:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ with col1:
 
 # Insert title in the second column
 with col2:
-    st.markdown("<h1 style='margin-top: 100px, font size = 100px;'>Respiratory Rate (RR) Monitoring Dashboard</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='margin-top: 100px; font-size: 100px;'>Respiratory Rate (RR) Monitoring Dashboard</h1>", unsafe_allow_html=True)
 
 # Placeholder for Date and Time
 datetime_placeholder = st.subheader("📅 Loading date and time...")
