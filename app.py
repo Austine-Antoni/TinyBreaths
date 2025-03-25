c1fbc1911f2619afcde885c2cc26bba0cff5626b
Austine-Antoni <168243971+Austine-Antoni@users.noreply.github.com>
2025-03-25 15:25:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ col1, col2 = st.columns([1, 5])  # Adjust width ratio as needed
 
 # Insert image in the first column
 with col1:
-    st.image("TINY.png", width 200)  # Adjust width as needed
+    st.image("TINY.png", width = 200)  # Adjust width as needed
 
 # Insert title in the second column
 with col2:
